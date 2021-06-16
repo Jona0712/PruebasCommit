e1c8346c24da02c33df45c2956e03d813b88fe5e
Hilbert Perucho <hilbert.perucho@gmail.com>
2021-06-16 11:46:26 -0600

    Metodo de Recorrer
    Metodo de Recorrer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,17 @@
 import { Component } from '@angular/core';
+import Controlador from 'src/clases/Controlador';
+import Evaluar from 'src/clases/Evaluar';
+import { TablaSimbolos } from 'src/clases/TablaSimbolos/TablaSimbolos';
+import * as Analizador from '../clases/Analizar'
+import * as xpath from "../Analizadores/gramatica"
+import * as xml from "../Analizadores/XML";
+import {graphviz} from 'd3-graphviz';
+import {wasmFolder} from '@hpcc-js/wasm'
+import Nodo from 'src/clases/AST/Nodo';
+import * as ReXML from '../Analizadores/XmlReporteGramatica';
+import * as RexPath from '../Analizadores/xPathReporteGramatica';
+
+import * as vis from "vis";
 
 
 @Component({
@@ -18,7 +31,33 @@ export class AppComponent {
 
 
   recorrer(): void{
-    
+    let ana = new Analizador.Analizador();
+    if(this.entradaxml != ""){
+      console.log("Vamos a graficar");
+      let nodo_ast: Nodo= ana.recorrer(this.entradaxml);
+      let grafo = nodo_ast.GraficarSintactico();  //Aqui tenemos la cadena de graphviz para graficar
+      console.log(grafo);
+      
+      const container = document.getElementById("app");
+      var parsedData = vis.network.convertDot(grafo);
+
+      var data = {
+        nodes: parsedData.nodes,
+        edges: parsedData.edges
+      };
+
+      var options = parsedData.options;
+
+      options.layout = {
+        "hierarchical": true
+      }
+
+      options.nodes = {
+        color: "cyan"
+      };
+
+      var network = new vis.Network(container, data, options);
+    }
   }
 
   ejecutar():void {
