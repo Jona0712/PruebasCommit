df138f7108a12f3aa9194d230079795c74068803
Jonnathan Castillo <jcastillo@homeland.com.gt>
2021-06-16 09:03:40 -0600

    saf

---
+++ src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  
+  entradaxpath : string  = "";
+  consola : string = "";
+  entradaxml:string="";
+  htmlts: string ="";
+  htmlerrores: string ="";
+  reporteGramatical: string = "";
+  xpathRG: string = "";
+
+
+  recorrer(): void{
+    
+  }
+
+  ejecutar():void {
+   
+    
+  }
+  imprimirTabla() {
+    
+    
+    
+      
+  }
+
+  openPage(pageName,valor) {
+    
+  }
+
+}
