e02d7fa80e9b5c9f5a10383d89ee3a1d44f487f9
Jonnathan Castillo <jcastillo@homeland.com.gt>
2021-06-16 11:56:07 -0600

    asdfasdf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -84,6 +84,31 @@ export class AppComponent {
 
   openPage(pageName,valor) {
     
+    if(valor==1){
+      document.getElementById("tablasimbols").innerHTML = this.htmlts;
+    }else if (valor == 2){
+      document.getElementById("tablasimbols").innerHTML = this.htmlerrores;
+    }else if(valor==3){
+      this.recorrer();
+    }
+    
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+  
+    document.getElementById(pageName).style.display = "block";
+  
+  
   }
 
 }
